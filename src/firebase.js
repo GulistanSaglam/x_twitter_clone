939c69ae6db5a61fdade456124184ca6f12038f5
Gülistan Sağlam <99056956+GulistanSaglam@users.noreply.github.com>
2024-03-30 04:01:42 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,7 @@ import 'firebase/compat/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBDvI96fBLq8lZh4VqvbI4KeuDcgBnIF1I",
-    authDomain: "x-clone-31de4.firebaseapp.com",
-    projectId: "x-clone-31de4",
-    storageBucket: "x-clone-31de4.appspot.com",
-    messagingSenderId: "29301055944",
-    appId: "1:29301055944:web:11dbfeb88b826a8cfbe0fd"
+  // YOUR CONFIGURATION
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
